eb4a7baea3507a023bccec8c1d997c9a9194129b
Franco Rosato <sigma0934@gmail.com>
2023-11-22 12:28:45 +0100

    fix | esercizio 3

--- Esercizio 3/main.js
+++ Esercizio 3/main.js
@@ -8,18 +8,22 @@ const book = {
   book.rating = 6;
   
   // Aggiungo un metodo "increaseRating" per incrementare il rating (non superiore a 10)
-  function increaseRating (increase) {
+  book.increaseRating = function (increase) {
     this.rating += increase;
   
     // Imposto il valore massimo di rating a 10
-    this.rating = Math.min(this.rating, 10);
+    this.rating = Math.min(this.rating, 10);   
   };
-  
+  book.increaseRating(9);
+
   // Rimuovo la proprietà "year" dall'oggetto book
   delete book.year;
   
   // Stampo in console ogni proprietà dell'oggetto book
-  for (let chiave in book) {
-    console.log(chiave + ": " + book[chiave]);
+  for (let key in book) {
+    if (key !== "increaseRating") {
+      console.log(`${key}: ${book[key]}`);
+    }
   }
+  
   
